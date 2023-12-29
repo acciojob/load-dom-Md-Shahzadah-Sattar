54c078a6fa25cdd313cf99fe5742311302ac244a
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-29 23:20:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Student can write their JavaScript code here
+
+document.addEventListener('DOMContentLoaded', function () {
+    // This function will be executed when the DOM is fully loaded
+    let paragraph = document.createElement('p');
+    paragraph.textContent = 'Hello, this content is generated dynamically by JavaScript!';
+    document.body.appendChild(paragraph);
+    
+    // Add more JavaScript code as needed
+});
