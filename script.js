f675a7d2c44e446b55012547f18739f7a7f23ede
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-01 21:17:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This function will be executed when the DOM is fully loaded
     let paragraph = document.createElement('p');
-    paragraph.textContent = 'DOM load success';
+    paragraph.textContent = "DOM load success";
     document.body.appendChild(paragraph);
     
     // Add more JavaScript code as needed
