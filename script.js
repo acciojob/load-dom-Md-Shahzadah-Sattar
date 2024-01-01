05570fe68c6113d982188be09d2e99b93dff96d9
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-01 21:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This function will be executed when the DOM is fully loaded
     let paragraph = document.createElement('p');
-    paragraph.textContent = 'Hello, this content is generated dynamically by JavaScript!';
+    paragraph.textContent = 'DOM load success';
     document.body.appendChild(paragraph);
     
     // Add more JavaScript code as needed
